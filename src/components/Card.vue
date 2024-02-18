a712c82ae1a36f74295e607bce15d9fd39cc4e87
estecore <estecore@yandex.ru>
2024-02-18 17:18:30 +0400

    add Card and CardList components

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps({
+  imageUrl: String,
+  title: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+</script>
+
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      @click="onClickFavorite ? onClickFavorite() : () => {}"
+      class="absolute top-8 left-8"
+      :src="isFavorite ? '/like-2.svg' : '/like-1.svg'"
+      alt="favorite"
+    />
+    <img :src="imageUrl" alt="sneaker" />
+
+    <p class="mt-2">{{ title }}</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }} руб.</b>
+      </div>
+
+      <img
+        @click="onClickAdd ? onClickAdd() : () => {}"
+        :src="isAdded ? '/checked.svg' : '/plus.svg'"
+        alt="plus"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import Card from './Card.vue'
+const onClickAdd: () => void = () => {
+  alert('Добавить!')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Кроссовки Nike Blazer Mid Suede"
+      :price="12000"
+      :isAdded="false"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
